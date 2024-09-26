5c532ddd4862b8b80f89f65cec4340dec33a8a37
KAYKE ANGELO ALVES DE OLIVEIRA <164965963+eternoaprendz@users.noreply.github.com>
2024-09-26 19:23:09 -0300

    Add files via upload

--- Script-10.sql
+++ Script-10.sql
@@ -10,7 +10,7 @@ CREATE TABLE itens (
 	id_item int not null primary key auto_increment,
 	nome varchar(255),
 	comando_correto varchar(255),
-
+    descricao varchar(300),
 	id_cena_atual int,
 	id_cena_destino int,
 	FOREIGN KEY(id_cena_atual) REFERENCES cenas(id_cena)
@@ -25,9 +25,21 @@ CREATE TABLE saves (
     
    
    
+   CREATE TABLE inventario (
+    id_item INT NOT NULL,
+    id_save INT NOT NULL,
+    PRIMARY KEY (id_item, id_save),
+    FOREIGN KEY (id_item) REFERENCES itens(id_item),
+    FOREIGN KEY (id_save) REFERENCES saves(id_save)
+);
 
-ALTER TABLE itens ADD COLUMN descricao VARCHAR(255);
-ALTER TABLE itens ADD COLUMN descricao VARCHAR(255);
-
-   
+insert into cenas(id_cena, descricao) values (1, "Jorginho desejou comer uma LASANHA e ele tem uma ROUPA para ir ao MERCADO para comprar os ingredientes para já que estava de cuequinha!
+");
+insert into cenas(id_cena, descricao) values (2, "Depois de vestir uma roupa você vai para o MERCADO, mas você encontra uma PEDRA-AZUL brilhante no chão e você se interessa naquilo.
+");
+insert into cenas(id_cena, descricao) values (3, "Vou ver isso depois, você diz e a guarda e volta ao rumo ao MERCADO.");
+insert into cenas(id_cena, descricao) values (4, "Enquanto você ia para o mercado você se depara com um PORTAL no chão que acha estranho pois ele é da mesma cor da PEDRA-AZUL e tem um buraco no meio dele.");
+insert into cenas(id_cena, descricao) values (5, "Você coloca essa pedra no portal e ele se “abre” e te suga  para dentro dele onde encontra um mundo com uma LASANHA e um MONSTRO faminto mais a frente(sinissstro).");
+insert into cenas(id_cena, descricao) values (6, "Você deu a lasanha ao monstro e ele ficou agradecido e com isso ele abriu um PORTAL-AZUL para você!(outro…).");
+insert into cenas(id_cena, descricao) values (7, "Você acorda em seu quarto e descobre que tudo foi um sonho (ainda bemkk) mas com uma vontade de comer lasanha…");
    
